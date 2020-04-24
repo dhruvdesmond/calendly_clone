5306d10bad25dd74fef21c3c6e19648a81bcad0d
Dhruv <dhruvdesmond@gmail.com>
2020-04-25 01:44:09 +0530

    meal crud

--- calendly_clone/urls.py
+++ calendly_clone/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('<int:user_id>/',views.get_user,name ="get_user"),
     path('edit/<int:user_id>/',views.edit_user,name ="edit_user"),
     path('users/',views.get_all_users,name ="list_of_users"),
-    path('signup',views.add_user,name ="add_user")
+    path('signup/',views.add_user,name ="add_user"),
+    path('meal/<int:meal_id>/',views.get_meal_by_id,name ="get_meal_by_id"),
+    path('addmeal/',views.add_meal,name ="add_meal"),
+    path('meals/',views.get_meals,name ="get_meals")
+    
 ]
--- calendly_clone/views.py
+++ calendly_clone/views.py
@@ -1,36 +1,56 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,JsonResponse
 from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+import json
+import requests
 
-from .models import User
+from .models import User,Meal
 # Create your views here.
+
+
 def index(request):
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def get_user(request,user_id):
+def search(food_name):
+    # result = {}
+    HEADERS = {"x-app-id": "732661df",
+                "x-app-key":"80451c846f26dc21e903158a11fd29e9",
+                "Content-Type": "application/json"
+    }
+    url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+    query = {"query": food_name}
+    response = requests.post(url, headers=HEADERS, json=query)
+    data = json.loads(response.content.decode("utf-8"))
+    print(len(data['foods']))
+    return (data['foods'][0]['nf_calories'])
+def get_user(request, user_id):
     try:
         print(user_id)
-        name = User.objects.get(pk=user_id)
-        print(name.username)
+        user = User.objects.get(pk=user_id)
+        print(user.username)
     except User.DoesNotExist:
         raise Http404("User does not exit")
-    template = loader.get_template('calendly/index.html') 
+    meals = Meal.objects.filter(username=user.id)
 
-    return HttpResponse(template.render({'user': name}, request))
+    template = loader.get_template('calendly/user_detail.html')
+    return HttpResponse(template.render({'user': user,'meals':meals}, request))
 
-def edit_user(request,user_id):
+
+def edit_user(request, user_id):
     if request.method == "POST":
         data = request.POST
 
         password = data["password"]
         curr_calories = data["curr_calories"]
         totalCalories = data["total_calories"]
-        User.objects.filter(pk=user_id).update(password=password,curr_calories=curr_calories,total_calories=totalCalories)
+        User.objects.filter(pk=user_id).update(
+            password=password, curr_calories=curr_calories, total_calories=totalCalories)
         User().save()
-        return redirect("get_user",user_id)
+        return redirect("get_user", user_id)
     else:
         try:
             print(user_id)
@@ -42,8 +62,6 @@ def edit_user(request,user_id):
         return HttpResponse(template.render({'user': name}, request))
 
 
-
-
 def get_all_users(request):
     try:
         list_of_users = User.objects.all()
@@ -53,6 +71,7 @@ def get_all_users(request):
 
     return HttpResponse(template.render({'list_of_users': list_of_users}, request))
 
+
 def add_user(request):
     if request.method == "POST":
         data = request.POST
@@ -60,15 +79,58 @@ def add_user(request):
         userName = data["user_name"]
         passWord = data["password"]
         totalCalories = data["total_calories"]
-        User.objects.create(username=userName,password=passWord,curr_calories=0,total_calories=totalCalories,calories_exceeded=False)
+        User.objects.create(username=userName, password=passWord, curr_calories=0,
+                            total_calories=totalCalories, calories_exceeded=False)
         User().save()
         return redirect('/users/')
     else:
         template = "calendly/signup.html"
-        return render(request,template) 
+        return render(request, template)
+
+def add_meal(request):
+    if request.method == "POST":
+        data = request.POST
+        print(data)
+        meal_name = data["meal_name"]
+        calories = data["calories"]
+        user_name = data["user_name"]
+        get_user = User.objects.filter(username=user_name).first()
+        if not get_user:
+            return HttpResponse("Wrong user name")
+        if calories:
+            pass
+        else:
+            calories = search(meal_name)
+        curr_calories = get_user.curr_calories + int(calories)
+        if curr_calories >= get_user.total_calories:
+            return HttpResponse("Calories exceeded for =",get_user.id)
+        User.objects.filter(pk=get_user.id).update(curr_calories=curr_calories)
+        Meal.objects.create(meal_name=meal_name, calories=calories, username=get_user)
+
+        
+        return redirect('/meals')
+    else:
+        template = "calendly/add_meal.html"
+        return render(request, template)
 
 
-def result(request,user_id):
+def get_meal_by_id(request,meal_id):
+    try:
+        print(meal_id)
+        name = Meal.objects.get(pk=meal_id)
+    except Meal.DoesNotExist:
+        raise Http404("Meal does not exit")
+    template = loader.get_template('calendly/meal_detail.html')
+    return HttpResponse(template.render({'meal': name}, request))
+
+def get_meals(request):
+    try:
+        list_of_meals = Meal.objects.all()
+    except Meal.DoesNotExist:
+        raise Http404("Meals does not exit")
+    template = loader.get_template('calendly/display_all_meals.html')
+    return HttpResponse(template.render({'list_of_meals': list_of_meals}, request))
+
+def result(request, user_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response %user_id)
-
+    return HttpResponse(response % user_id)
